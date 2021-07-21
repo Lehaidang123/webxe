0c3858708cf115b8d85210ad32cc2435a78e1e28
Hai Dang <lehaidangxh@gmail.com>
2021-07-21 20:50:02 +0700

    commit

--- Controllers/adminController.cs
+++ Controllers/adminController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using System.Net;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.UI;
 
 namespace DCXEMAY.Controllers
 {
@@ -24,6 +28,35 @@ namespace DCXEMAY.Controllers
         {
             SanPham sanPham = new SanPham();
             return View(db.SanPhams.ToList());
+        }
+        public ActionResult oderdetail()
+        {
+          
+            return View(db.Oderdetails.ToList());
+        }
+        public ActionResult ExportToExcel()
+        {
+            var gv = new GridView();
+            var g =new GridView();
+            g.DataSource = db.Oderdetails.ToList();
+            gv.DataSource = db.Oders.ToList();  
+          
+            gv.DataBind();
+            g.DataBind();
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=DemoExcel.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "utf-8";
+            StringWriter objStringWriter = new StringWriter();
+            HtmlTextWriter objHtmlTextWriter = new HtmlTextWriter(objStringWriter);
+            gv.RenderControl(objHtmlTextWriter);
+            g.RenderControl(objHtmlTextWriter);
+
+            Response.Output.Write(objStringWriter.ToString());
+            Response.Flush();
+            Response.End();
+            return View("admin");
         }
     }
 }
